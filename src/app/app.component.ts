b9de77ada4a8f211358e3689c55bc57f2acd3d39
john Cervantes <johncamilocervantes@gmail.com>
2021-12-30 12:47:32 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import productsJson from '../shared/data.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular-products';
+  products = [...productsJson];
+  featuredProduct = productsJson[0];
+  productHover = 0;
+  hover = false;
+
+  constructor() {
+    for (let i = 0; i < this.products.length; i++) {
+      this.products[i].date = new Date(this.products[i].date)
+        .toString()
+        .slice(0, 10);
+    }
+  }
+
+  setFeaturedProduct(index: number) {
+    this.featuredProduct = this.products[index];
+  }
+
+  onMouseEnter(index: number) {
+    this.productHover = index;
+    console.log(index);
+  }
 }
